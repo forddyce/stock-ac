e444ff1b4eb11f4eaacfe9287962efb22d0954c4
Fordyce Gozali <forddyce92@gmail.com>
2026-01-01 13:23:29 +0700

    cleanup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,66 +11,5 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolePermissionSeeder::class,
         ]);
-
-        $admin = User::firstOrCreate(
-            ['email' => 'admin@abc-elektronik.com'],
-            [
-                'name' => 'Administrator',
-                'password' => Hash::make('MasterPassword123!'),
-                'email_verified_at' => now(),
-            ]
-        );
-        $admin->assignRole('admin');
-
-        $admin = User::firstOrCreate(
-            ['email' => 'aively@abc-elektronik.com'],
-            [
-                'name' => 'Administrator',
-                'password' => Hash::make('password'),
-                'email_verified_at' => now(),
-            ]
-        );
-
-        $admin->assignRole('admin');
-
-        $purchaseOp = User::firstOrCreate(
-            ['email' => 'purchase@abc-elektronik.com'],
-            [
-                'name' => 'Purchase Operator',
-                'password' => Hash::make('password'),
-                'email_verified_at' => now(),
-            ]
-        );
-        $purchaseOp->assignRole('purchase_operator');
-
-        $salesOp = User::firstOrCreate(
-            ['email' => 'sales@abc-elektronik.com'],
-            [
-                'name' => 'Sales Operator',
-                'password' => Hash::make('password'),
-                'email_verified_at' => now(),
-            ]
-        );
-        $salesOp->assignRole('sales_operator');
-
-        $salesPurchaseOp = User::firstOrCreate(
-            ['email' => 'andriani@abc-elektronik.com'],
-            [
-                'name' => 'Andriani',
-                'password' => Hash::make('password'),
-                'email_verified_at' => now(),
-            ]
-        );
-        $salesPurchaseOp->assignRole('sales_purchase_operator');
-
-        $salesPurchaseOp = User::firstOrCreate(
-            ['email' => 'fionavera@abc-elektronik.com'],
-            [
-                'name' => 'Fiona Vera Jayanti',
-                'password' => Hash::make('password'),
-                'email_verified_at' => now(),
-            ]
-        );
-        $salesPurchaseOp->assignRole('sales_purchase_operator');
     }
 }
